8268f0b717bb8598045716dc8e68b23e4cf4b02b
Abdalla Shaaban <abdallah.shaaban@cis.asu.edu.eg>
2018-03-11 17:31:25 +0200

    1

---
+++ Task1/Startup code/SecurityLibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+        public string Encrypt(string plainText, int key)
+        {
+            string cipherText="";
+            for (int i = 0; i < plainText.Length; ++i) {
+
+                cipherText += (char)(((((plainText[i] - 'a') + key) % 26) + 'A'));
+
+            }
+            return cipherText;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            string plainText = "";
+            for (int i = 0; i < cipherText.Length; ++i)
+            {
+                plainText += (char)(((((cipherText[i] - 'A') - key) + 26) % 26) + 'a');
+
+            }
+            return plainText;
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            int key = (((cipherText[0]-'A') - (plainText[0]-'a')) + 26) % 26;
+            return key;
+        }
+    }
+}
