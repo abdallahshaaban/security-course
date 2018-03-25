76372610902ce993be33980cbc1b9b4b0e663b6f
AbdullahMahmoud <abdullahfcis@yahoo.com>
2018-03-25 22:53:13 +0200

    .

--- Task3/StartupCode/SecurityLibrary/AES/AES.cs
+++ Task3/StartupCode/SecurityLibrary/AES/AES.cs
@@ -9,16 +9,190 @@ namespace SecurityLibrary.AES
     /// <summary>
     /// If the string starts with 0x.... then it's Hexadecimal not string
     /// </summary>
+    
     public class AES : CryptographicTechnique
     {
+        byte[,] SBox = new byte[16, 16] {  // populate the Sbox matrix
+    /*       0     1     2     3     4     5     6     7     8     9     a     b     c     d     e     f */
+    /*0*/  {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76},
+    /*1*/  {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0},
+    /*2*/  {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15},
+    /*3*/  {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75},
+    /*4*/  {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84},
+    /*5*/  {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf},
+    /*6*/  {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8},
+    /*7*/  {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2},
+    /*8*/  {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73},
+    /*9*/  {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb},
+    /*a*/  {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79},
+    /*b*/  {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08},
+    /*c*/  {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a},
+    /*d*/  {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e},
+    /*e*/  {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf},
+    /*f*/  {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16} };
+        byte[,] Rcon = new byte[4, 10] {
+        {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1b,0x36},
+        {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+        {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+        {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}};
+        byte[,] M = new byte[4, 4] {
+        {0x02,0x03,0x01,0x01},
+        {0x01,0x02,0x03,0x01},
+        {0x01,0x01,0x02,0x03},
+        {0x03,0x01,0x01,0x02}};
         public override string Decrypt(string cipherText, string key)
         {
             throw new NotImplementedException();
         }
-
         public override string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            byte[,] bplainText = StringToMatrixOfBytes(plainText);
+            byte[,] bKey = StringToMatrixOfBytes(key);
+            //Round 0
+            bplainText = AddRoundkey(bplainText, bKey);
+            //Rounds 1 to 9
+            for(int i = 1; i<=9; i++)
+            {
+                bKey = GenerateRoundKey(bKey, i);
+                bplainText = AddRoundkey(MixColumns(ShiftRows(SubBytes(bplainText))), bKey);
+            }
+            //Round 10
+            bKey = GenerateRoundKey(bKey, 10);
+            bplainText = AddRoundkey(ShiftRows(SubBytes(bplainText)), bKey);
+
+            return MatrixOfBytesToString(bplainText);
+        }
+        private byte[,] MixColumns(byte[,] bplainText)
+        {
+            byte[,] bytes = new byte[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    byte Res = 0x00;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        byte tmp;
+                        if (M[i, k] == 0x03)
+                        {
+                            tmp = (byte)(bplainText[k, j] << 1);
+                            if ((byte)(bplainText[k, j] & 0x80) == 0x80)
+                            {
+                                tmp = (byte)((int)tmp ^ (int)(0x1b));
+                            }
+                            tmp = (byte)((int)tmp ^ (int)bplainText[k, j]);
+                        }
+                        else if (M[i, k] == 0x02)
+                        {
+                            tmp = (byte)(bplainText[k, j] << 1);
+                            if ((byte)(bplainText[k, j] & 0x80) == 0x80)
+                            {
+                                tmp = (byte)((int)tmp ^ (int)(0x1b));
+                            }
+                        }
+                        else
+                        {
+                            tmp = bplainText[k, j];
+                        }
+                        Res = (byte)((int)Res ^ (int)tmp);
+                    }
+                    bytes[i, j] = Res;
+                }
+            }
+            return bytes;
+        }
+        private byte[,] ShiftRows(byte[,] bplainText)
+        {
+            byte[,] bytes = new byte[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (j + i < 4)
+                        bytes[i, j] = bplainText[i, j + i];
+                    else
+                        bytes[i, j] = bplainText[i, (j + i) - 4];
+                }
+            }
+            return bytes;
+        }
+        private byte[,] SubBytes(byte[,] bplainText)
+        {
+            byte[,] bytes = new byte[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bytes[i, j] = SBox[bplainText[i, j] >> 4, bplainText[i, j] & 0x0f];
+                }
+            }
+            return bytes;
+        }
+        private byte[,] GenerateRoundKey(byte[,] bKey, int round)
+        {
+            byte[,] bytes = new byte[4, 4];
+            bytes[0, 0] = SBox[bKey[1, 3] >> 4, bKey[1, 3] & 0x0f];
+            bytes[1, 0] = SBox[bKey[2, 3] >> 4, bKey[2, 3] & 0x0f];
+            bytes[2, 0] = SBox[bKey[3, 3] >> 4, bKey[3, 3] & 0x0f];
+            bytes[3, 0] = SBox[bKey[0, 3] >> 4, bKey[0, 3] & 0x0f];
+            for (int i = 0; i < 4; i++)
+            {
+                bytes[i, 0] = (byte)((int)bytes[i, 0] ^ (int)Rcon[i, round - 1]);
+                bytes[i, 0] = (byte)((int)bytes[i, 0] ^ (int)bKey[i, 0]);
+            }
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++) bytes[j, i] = (byte)((int)bytes[j, i - 1] ^ (int)bKey[j, i]);
+            }
+            return bytes;
+        }
+        private byte[,] AddRoundkey(byte[,] bplainText , byte[,] bKey)
+        {
+            byte[,] bytes = new byte[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bytes[j, i] = (byte)((int)bplainText[j, i] ^ (int)bKey[j, i]);
+                }
+            }
+            return bytes;
+        }
+        private byte[,] StringToMatrixOfBytes(string HexStr)
+        {
+            string str = HexStr.Substring(2, HexStr.Length - 2);
+            byte[] Temp = Enumerable.Range(0, str.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(str.Substring(x, 2), 16))
+                             .ToArray();
+            byte[,] bytes = new byte[4, 4];
+            int c = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bytes[j, i] = Temp[c];
+                    c++;
+                }
+            }
+            return bytes;
+        }
+        private string MatrixOfBytesToString(byte[,] Mbytes)
+        {
+            byte[] bytes = new byte[16];
+            int c = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bytes[c] = Mbytes[j, i];
+                    c++;
+                }
+            }
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+                hex.AppendFormat("{0:x2}", b);
+            return "0x" + hex.ToString();
         }
     }
 }
